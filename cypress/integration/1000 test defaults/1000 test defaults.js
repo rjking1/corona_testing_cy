24574066dedd282ffd3fc26585ba5fe0faf559a4
Richard King <richard@analytics.com.au>
2021-08-27 09:16:24 +1000

    reformat; allow waits

--- cypress/integration/1000 test defaults/1000 test defaults.js
+++ cypress/integration/1000 test defaults/1000 test defaults.js
@@ -1,13 +1,17 @@
 import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
-import { compareFiles } from "../../../../common_cy/utils.js"
+import { compareFiles } from "../../../../common_cy/utils.js";
 
 Then("click on {string} and wait", (el) => {
-    cy.contains(el).click();
-    cy.wait(700);
+  cy.contains(el).click();
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
+  cy.wait(700);
 });
 
 Then("check history", () => {
-    cy.get(".apexcharts-menu-icon").click();
-    cy.contains("Download CSV").click();
-    compareFiles("./cypress/downloads/ca-corona.csv", "./cypress/expected/expected-ca-corona.csv")
+  cy.get(".apexcharts-menu-icon").click();
+  cy.contains("Download CSV").click();
+  compareFiles(
+    "./cypress/downloads/ca-corona.csv",
+    "./cypress/expected/expected-ca-corona.csv"
+  );
 });
--- cypress/integration/common/corona_common.js
+++ cypress/integration/common/corona_common.js
@@ -2,8 +2,9 @@ import { And, Given, Then, When } from "cypress-cucumber-preprocessor/steps";
 
 Given("I open the corona app", () => {
   cy.visit(Cypress.env("CORONA_URL"));
+  // eslint-disable-next-line cypress/no-unnecessary-waiting
   cy.wait(1000);
-})
+});
 
 Then("click on element {string}", (el) => {
   cy.get(el).click();
@@ -13,11 +14,11 @@ Then("click on {string}", (txt) => {
   cy.contains(txt).click();
 });
 
-Then("enter {string} into {string}", (txt,id) => {
+Then("enter {string} into {string}", (txt, id) => {
   cy.get(id).focus().clear().type(txt);
 });
 
 Then("check {string} has text {string}", (el, txt) => {
   // have.text does not work. why?
-  cy.get(el).should('have.value', txt)
-})
+  cy.get(el).should("have.value", txt);
+});
